07ed7ef05e6751494a947e1bd78b9cae8553a164
Hita-Haru <satmoti@outlook.com>
2025-08-14 14:02:44 +0900

    a

--- app/__init__.py
+++ app/__init__.py
@@ -17,8 +17,7 @@ def create_app():
 
 	from .models import User
 	@login_manager.user_loader
-
-def load_user(user_id):
+	def load_user(user_id):
 		return User.query.get(int(user_id))
 
 	from .routes import main as main_blueprint
